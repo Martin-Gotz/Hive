117134f6d6b3c90f950ada6d71a02f4df50d966c
Youssef <Youssef@LAPTOP-FQ2RFL0E>
2024-11-09 12:34:54 +0100

    introduction aux nouveaux éléments du plateau fonction affichervoisins à compléter, surement des erreurs (manque de const qlq part ou des conneries du genre)

--- include/Plateau.h
+++ include/Plateau.h
@@ -1,11 +1,48 @@
 #pragma once
+#include <iostream>
+#include <vector>
 
+using namespace std;
 namespace Hive {
+	//on définit dans un premier temps une seule cellule
+	class Cellule
+	{
+	private:
+		int x;
+		int y;
+	public :
+		int get_X() const { return x; }
+		int get_Y() const { return y; }
+		Cellule(int coord_x, int coord_y) : x(coord_x), y(coord_y) {}
+		vector<Cellule> getVoisins() const;
+		void afficherVoisins() const;
+	};
+
 	class Plateau
 	{
 	private:
-
+		int ID;
+		int nombre_cellules;
+		vector<Cellule> Cellules;
 	public:
+		vector<Cellule> getCellules() const;
+		Plateau() :nombre_cellules(0) {};
+		friend ostream& operator<<(ostream& f, Plateau& p);
+		int getId() const { return ID; }
+		int getNombreCellules() const { return nombre_cellules; }
+	};
+	ostream& operator<<(ostream& f, const Plateau& p);
+
+	class Ensemble_Plateaux
+	{
+	private:
+		vector<Plateau> plateau;
+		int nombre;
+	public:
+		Ensemble_Plateaux();
+		~Ensemble_Plateaux();
+		Plateau getPlateau(int ID);
+		int getNombrePlateau() const;
 	};
 }
 
--- src/Plateau.cpp
+++ src/Plateau.cpp
@@ -1,1 +1,34 @@
 #include "../include/Plateau.h"
+
+using namespace Hive;
+
+	vector<Cellule> Plateau::getCellules() const
+	{
+		return Cellules;
+	}
+
+	vector<Cellule> Cellule::getVoisins() const
+	{
+		vector<Cellule> Voisins;
+		vector<pair<int, int>> directions = {
+		{ 1, 0 }, { 1, -1 }, { 0, -1 },
+		{ -1, 0 }, { -1, 1 }, { 0, 1 }
+		};
+		for (auto dir : directions)
+		{
+			Voisins.emplace_back(x + dir.first, y + dir.second);
+		}
+		return Voisins;
+	}
+
+	ostream& operator<<(ostream& f, const Plateau& p)
+	{
+		f << "Les cellules dans le plateau numéro : " << p.getId() << "\n";
+		f << "Nombre de cellules : " << p.getNombreCellules() << "\n";
+		int i = 0;
+		for (const auto& cell : p.getCellules())
+		{
+			f << "Case numéro : " << i++ << " Coordonnées : (" << cell.get_X() << cell.get_Y() << ")\n";
+		}
+		return f;
+	}
